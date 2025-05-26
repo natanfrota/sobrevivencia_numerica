e122cb2970444319fce5ca69c08c8566a400ca28
SanchoArruda <gucairesarruda@gmail.com>
2025-05-26 16:54:39 -0300

    Adição de novas funcionalidades

--- Juiz.java
+++ Juiz.java
@@ -7,19 +7,50 @@ import java.io.*;
 
 public class Juiz {
     final int NUMERO_DE_JOGADORES = 3;
-    //Jogador[] jogadores = new Jogador[NUMERO_DE_JOGADORES]; // usando vetor
-    private Map<SocketAddress, Jogador> jogadores = null; // usando map
-    private DatagramSocket soqueteServidor = null;
+    //Jogador[] jogadores = new Jogador[NUMERO_DE_JOGADORES];
+    Map<SocketAddress, Jogador> jogadores = null;
+    DatagramSocket soqueteServidor = null;
     
     public Juiz(DatagramSocket soqueteServidor){
         this.soqueteServidor = soqueteServidor;
         this.jogadores = new HashMap<>();
     }
 
-    public void receberNumeroEscolhidos() throws IOException{
-        System.out.println("Aguardando os numeros escolhidos...");
-        int jogadorComNumero = 0;
-        while(jogadorComNumero < NUMERO_DE_JOGADORES){
+    public void aguardarJogadores() throws IOException {
+        System.out.println("Aguardando os jogadores...");
+
+        while (this.jogadores.size() < this.NUMERO_DE_JOGADORES) {
+            System.out.println(this.jogadores.size() + " jogadores encontrados...");
+
+
+            byte[] dado = new byte[50]; // armazena o nome do jogador que vem na requisição
+            DatagramPacket requisicao = new DatagramPacket(dado, dado.length);
+            this.soqueteServidor.receive(requisicao);
+
+            // cria um novo jogador
+            Jogador novoJogador = new Jogador(new String(requisicao.getData()), requisicao.getSocketAddress());
+
+            // armazena-o
+            // jogadores[contJogadores] = novoJogador;
+            this.jogadores.put(requisicao.getSocketAddress(), novoJogador);
+
+            System.out.println("Jogador(a): " + novoJogador.getNome() + " entrou no jogo.");
+            System.out.println("IP: " + requisicao.getAddress());
+            System.out.println("Porta: " + requisicao.getPort());
+
+            /* // deixei isso no lado do cliente
+            
+            byte[] mensagem = "Aguardando jogadores oponentes...".getBytes();
+            DatagramPacket resposta = new DatagramPacket(mensagem, mensagem.length, novoJogador.getEnderecoSoquete());
+
+            // envia a mensagem para o cliente
+            this.soqueteServidor.send(resposta); */
+        }
+    }
+
+    public void receberNumerosEscolhidos() throws IOException{
+        int contadorDeNumeros = 0;
+        while(contadorDeNumeros < this.NUMERO_DE_JOGADORES){
            byte[] buffer = new byte[50]; 
            DatagramPacket pacote = new DatagramPacket(buffer, buffer.length); // cria um pacote UDP para receber a mensagem e liga esse pacote a um vetor
            this.soqueteServidor.receive(pacote); //espera a msg e quando chegar armazena dentro do vetor num
@@ -30,52 +61,141 @@ public class Juiz {
            SocketAddress endereço = pacote.getSocketAddress();
            Jogador jogador = jogadores.get(endereco);
 
-           if(jogador != null && jogador.getNumeroEscolhido() == 0){
+           if(jogador != null){
                 jogador.setNumeroEscolhido(numero); // armazena o numero que ele enviou no objeto jogador
-                jogadorComNumero++; // contar quantos envaram e serve para parar o for 
+                contadorDeNumeros++; // contar quantos envaram e serve para parar o for 
                 System.out.println(jogador.getNome() + " escolheu: " + numero);
                  // msg de confirmaçao
                  byte[] confirmacao = "Numero recebido".getBytes();
                  // envia o pacote udp de volta para o endereço do jogador que enviou o numero
                  soqueteServidor.send(new DatagramPacket(confirmacao, confirmacao.length, endereco));
-
            }
 
         }
     }
 
-    public void aguardarJogadores() throws IOException {
-        System.out.println("Aguardando os jogadores...");
+    public void iniciarJogo() throws IOException{
+        if(this.jogadores.size() != this.NUMERO_DE_JOGADORES)
+            return;
 
-        while (this.jogadores.size() < this.NUMERO_DE_JOGADORES) {
+        // avisa aos jogadores que o jogo vai começar
+        enviarConfirmacao();
 
-            byte[] dado = new byte[50]; // armazena o nome do jogador que vem na requisição
-            DatagramPacket requisicao = new DatagramPacket(dado, dado.length);
-            this.soqueteServidor.receive(requisicao);
+        // recebe os números dos jogadores enquanto houver três jogadores; pois, ao sair um, as regras mudam
+        System.out.println("Aguardando os números de cada jogador...");
+        
+        // enquanto isso for verdadeiro, o jogo tem três jogadores
+        while(this.jogadores.size() == this.NUMERO_DE_JOGADORES){
+            receberNumerosEscolhidos();
 
-            // cria um novo jogador
-            Jogador novoJogador = new Jogador(new String(requisicao.getData()), requisicao.getSocketAddress());
+            System.out.println("Realizando cálculo da média...");
+            double media = this.calcularMedia();
+            System.out.println("Média encontrada...");
+            System.out.printf("\nMédia: %.2f\n", media);
 
-            // armazena-o
-            // jogadores[contJogadores] = novoJogador; // usando vetor
-            this.jogadores.put(requisicao.getSocketAddress(), novoJogador); // usando map
+            System.out.println("\nRealizando cálculo do valor alvo...");
+            double valorAlvo = this.calcularValorAlvo(media); 
+            System.out.println("Valor alvo encontrado...");
+            System.out.printf("\nValor alvo: %.2f\n", valorAlvo);
+            
+            /* comparar o valor alvo com o numero escolhido por cada um */
+            
+            // j1 - distancia 75 - perde 2 -- maior distância
+            // j2 - distancia 12 - não perde nada -- distância menor
+            // j3 - distancia 35 - perde 1 -- distância média
+            
+            System.out.println("Realizando cálculo do placar do jogo...");
+            this.calcularPlacar(valorAlvo);
 
-            System.out.println("Jogador(a): " + novoJogador.getNome() + " entrou no jogo.");
-            System.out.println("IP: " + requisicao.getAddress());
-            System.out.println("Porta: " + requisicao.getPort());
+            System.out.println("Atualizando quantidade de jogadores...");
 
-            byte[] mensagem = "Aguardando jogadores oponentes...".getBytes();
-            DatagramPacket resposta = new DatagramPacket(mensagem, mensagem.length, novoJogador.getEnderecoSoquete());
+            String[] posicoes = {"primeiro", "segundo", "terceiro"};
+            int i = 0;
+            for(Jogador jogador : this.jogadores.values()){
+                System.out.println("Enviando placar para " + posicoes[i] + " jogador " + jogador.getPontuacao());
 
-            // envia a mensagem para o cliente
-            this.soqueteServidor.send(resposta);
+                byte[] placar = String.valueOf(jogador.getPontuacao()).getBytes();
+                DatagramPacket resposta = new DatagramPacket(placar, placar.length, jogador.getEnderecoSoquete());
+                soqueteServidor.send(resposta);
+
+                i++;
+            }
+
+            System.out.println("Verificando se algum jogador foi eliminado...");
+
+            SocketAddress chave = null;
+            for (Jogador jogador : this.jogadores.values()) {
+                if(jogador.getPontuacao() <= -6){
+                    chave = jogador.getEnderecoSoquete();
+                }
+            }
+            if(chave != null){
+                Jogador jogador = this.jogadores.remove(chave);
+                System.out.println("Jogador(a) " + jogador.getNome() + " foi eliminado(a).");
+            }
+
+        }
+
+        // quando houver apenas dois
+    }
+
+    private void enviarConfirmacao() throws IOException {
+        byte[] confirmacao = "Jogadores oponentes encontrados...\n\nQue comecem os jogos...".getBytes();
+
+        for (Jogador jogador : jogadores.values()) {
+            DatagramPacket resposta2 = new DatagramPacket(confirmacao, confirmacao.length, jogador.getEnderecoSoquete());
+            this.soqueteServidor.send(resposta2);
         }
     }
 
+    private double calcularMedia(){
+        int soma = 0;
 
+        for(Jogador jogador : jogadores.values()){
+            soma += jogador.getNumeroEscolhido();
+        }
 
+        double media = (double) soma / this.jogadores.size();
+        
+        return media;
+    } 
 
-    
+    private double calcularValorAlvo(double media){
+        return media * 0.8;
+    }
+
+    public void calcularPlacar(double valorAlvo){
+        Jogador[] tempJogadores = this.jogadores.values().toArray(new Jogador[this.jogadores.size()]);
+
+        for (int i = 0; i < tempJogadores.length - 1; i++){
+            for (int j = i + 1; j < tempJogadores.length; j++){
+                if(tempJogadores[i].calcularDiferença(valorAlvo) > tempJogadores[j].calcularDiferença(valorAlvo)){
+                    Jogador temp = tempJogadores[i];
+                    tempJogadores[i] = tempJogadores[j];
+                    tempJogadores[j] = temp;
+                }
+            }
+        }
+
+        for (Jogador jogador : tempJogadores) {
+            System.out.printf("Distância do valor alvo do(a) jogador(a) - %s = %f\n", jogador.getNome(), jogador.calcularDiferença(valorAlvo));
+        }
+
+        /* mudar a pontuacao de cada jogador */
+        // jogador com maior distância
+        System.out.println("Jogador(a) " + tempJogadores[2].getNome() + " receberá -2 pontos.");
+        tempJogadores[2].setPontuacao(tempJogadores[2].getPontuacao() - 2);
+
+        // jogador com menor distância
+        System.out.println("Jogador(a) " + tempJogadores[0].getNome() + " receberá 0 ponto.");
+        tempJogadores[0].setPontuacao(tempJogadores[0].getPontuacao() - 0);
+
+        // jogador com distância média
+        System.out.println("Jogador(a) " + tempJogadores[1].getNome() + " receberá -1 ponto.");
+        tempJogadores[1].setPontuacao(tempJogadores[1].getPontuacao() - 1);
+
+    }
+
     public void finalizarJogo(){
         this.soqueteServidor.close();
     }
@@ -91,9 +211,9 @@ public class Juiz {
             DatagramSocket soqueteServidor = new DatagramSocket(NUMERO_DA_PORTA);
             juiz = new Juiz(soqueteServidor);
             juiz.aguardarJogadores();
-            
-
+            juiz.iniciarJogo();
             juiz.finalizarJogo();
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
